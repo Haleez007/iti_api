a22db0c501ce7c0f6e75b01c791f67988e473182
h3leez <h3leez@icloud.com>
2025-08-28 00:21:53 +0300

    haliz

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'food_login.dart';
+
+void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  runApp(const MyApp());
+  print("start");
+  Timer( Duration(seconds: 5), () {
+    print("end");
+
+    FlutterNativeSplash.remove();
+  }
+  );}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        fontFamily: 'VendSans',
+
+      ),
+      home: FoodLogin(),
+    );
+  }
+}
