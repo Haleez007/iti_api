8bc80db0cffa026583cde3155670d72f5b23a5ff
h3leez <h3leez@icloud.com>
2025-08-28 00:21:53 +0300

    haleez

---
+++ lib/onboarding_model.dart
@@ -0,0 +1,15 @@
+List onboardingImages = [
+  "assets/images/onbording1.svg",
+  "assets/images/onbording2.svg",
+  "assets/images/onbording3.svg",
+];
+List onboardingTitles = [
+  "Choose Products",
+  "Make Payment",
+  "Get Your Order",
+];
+List onboardingSubTitles = [
+  "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.",
+  "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.",
+  "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.",
+];
