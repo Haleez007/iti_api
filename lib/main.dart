348966016318f5b081fb3affd5cabde6290cc120
h3leez <h3leez@icloud.com>
2025-08-28 00:21:53 +0300

    haleez

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'cubit/counter_cubit.dart';
+import 'image_picker.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Image Picker with Counter',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const ImagePickerScreen(),
+      ),
+    );
+  }
+}
